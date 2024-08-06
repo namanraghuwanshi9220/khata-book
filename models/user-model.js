ebdbacc21860dc6bf1d3241a3b9411bef77a0856
Naman raghuwanshi <namanraghuwanshi789@gmail.com>
2024-08-06 22:50:19 +0530

    Add profile directory

--- models/user-model.js
+++ models/user-model.js
@@ -14,6 +14,6 @@ const userSchema = mongoose.Schema({
         select: false,
    },
     hisaab: [{  type: mongoose.Schema.Types.ObjectId, ref: "hisaab"  }],
-}) 
+}); 
 
 module.exports = mongoose.model("user", userSchema);
